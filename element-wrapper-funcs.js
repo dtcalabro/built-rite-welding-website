877f60c8971f0432065225ab0daac46da2738a41
dtcalabro <45155421+dtcalabro@users.noreply.github.com>
2023-07-15 19:13:11 -0400

    Added new prototype function called replaceClassIfPresent
    This should fix the issue where it would throw the error 'Uncaught TypeError: formAlert.replaceClassIfPresent is not a function'

--- element-wrapper-funcs.js
+++ element-wrapper-funcs.js
@@ -18,3 +18,10 @@ Element.prototype.removeClassIfPresent = function(classname) {
 Element.prototype.containsClass = function(classname) {
     return this.classList.contains(classname);
 }
+
+Element.prototype.replaceClassIfPresent = function(origClassname, newClassname) {
+    if (this.classList.contains(origClassname)) {
+        this.classList.remove(origClassname);
+        this.classList.add(newClassname);
+    }
+}
